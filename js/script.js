c7802d0fc1ebd9197a37b7a2020264f916aec431
shaun2416 <shaunwhyte44@gmail.com>
2019-09-26 16:49:16 +1200

    removed console logging

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ document.getElementById('submitButton').onclick = function(){
 	  let num2 = (document.getElementById('number2').value);
 
 	  var lastChar = num2[num2.length - 1];
-	  console.log(lastChar);
 	  let result = '';
 	  if(lastChar == '6'){
 			result =  (num1 + parseFloat(num2)); 
@@ -12,8 +11,8 @@ document.getElementById('submitButton').onclick = function(){
 			result = "ERROR"; 
 	  }else{
 		  
-		  num2 = parseFloat(num2);
-			result = parseFloat( num1 * (num2 ** -1)).toFixed(3);
+		num2 = parseFloat(num2);
+		result = parseFloat( num1 * (num2 ** -1)).toFixed(3);
 	  }
 	  
 	  document.getElementById('result').value = result;
