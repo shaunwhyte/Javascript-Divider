ebbce69a05393f9887fa678c9eae689bd98a9cfc
shaun2416 <shaunwhyte44@gmail.com>
2019-09-26 16:46:07 +1200

    little fix

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 
 document.getElementById('submitButton').onclick = function(){
 	  let num1 = parseFloat(document.getElementById('number1').value);
-	  let num2 = parseFloat(document.getElementById('number2').value);
+	  let num2 = (document.getElementById('number2').value);
 
 	  var lastChar = num2[num2.length - 1];
+	  console.log(lastChar);
 	  let result = '';
 	  if(lastChar == '6'){
 			result =  (a + b); 
 	  }else if(lastChar == '2'){
 			result = "ERROR"; 
 	  }else{
+		  
+		  num2 = parseFloat(num2);
 			result = parseFloat( num1 * (num2 ** -1)).toFixed(3);
 	  }
 	  
